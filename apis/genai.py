86933b00c3da25419ea7bb2d6330a98d281ca43c
Bryan Alexis <bryalexis.ba@gmail.com>
2025-07-02 21:09:04 +0000

    Finished? genai implementation

--- apis/genai.py
+++ apis/genai.py
@@ -5,23 +5,31 @@ from google.genai import types
 # Set environment variables
 my_api_key = os.getenv('GENAI_KEY')
 genai.api_key = my_api_key
-
 # Create an genAI client using the key from our environment variable
 client = genai.Client(
     api_key=my_api_key,
 )
 
-# Specify the model to use and the messages to send
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    config=types.GenerateContentConfig(
-      system_instruction="Give constructive feedback on the results of the resume to job match based on the score/tier. If in tier 1, highlight what skills gave the user a high rating. If in other two, tell user which keywords are missing"
-    ),
-    contents="What made the resume a good match or poor match?",
-)
-
-print(response.text)
-
-"""
-Give results of rankings and scores for feedback
-"""
+# Give results of rankings and scores for feedback
+def feedback(resume, score, tier, job_title, job_description):
+    # Specify the model to use and the messages to send
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        config=types.GenerateContentConfig(
+        system_instruction=
+            f"You're a resume assistant helping the user understand why their resume was matched with the following job with a score of {score}% and tier: {tier}"
+        ),
+        contents= f"""
+            Job Title: {job_title}
+            Job Description:
+            """
+            {job_description}
+            """
+            Resume:
+            """
+            {resume}
+            """
+            """,
+        #contents="What made the resume a good match or poor match?",
+    )
+    return response.text.strip()
