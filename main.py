0f5a9eed95cd38039bc112a5ea20d1d3688954b5
Bryan Alexis <bryalexis.ba@gmail.com>
2025-07-02 21:11:23 +0000

    Changes

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-import requests
+from apis.resumeapi import getResume, topFiveMatches
+# from apis.jobapi import
 
+resume_text = getResume()
+jobs = 
+
+top_matches = topFiveMatches(resume_text, jobs)
