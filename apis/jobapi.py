3f03fe327eb48f36a0d99454ad6c5fda881268a3
Justin Oketunmbi <okedaramola1@gmail.com>
2025-07-02 18:11:56 +0000

    implemented secured auth for job api

--- apis/jobapi.py
+++ apis/jobapi.py
@@ -1,2 +1,24 @@
 import requests
+import os
+
+auth_key = os.environ.get('USAJOB_AUTH_KEY')
+user_agent = os.environ.get('USAJOB_USER_AGENT')
+
+if not auth_key or not auth_key:
+    raise ValueError("missing auth-key or user-agent")
+
+headers = {
+    "User-Agent": user_agent,
+    "Authorization-Key": auth_key
+}
+
+params = {
+    "Keyword": "software engineer"
+}
+
+url = "https://data.usajobs.gov/api/search"
+response = requests.get(url, headers=headers, params=params)
+
+
+print(response.json())
 
